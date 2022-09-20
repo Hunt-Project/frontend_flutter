19dcbc15c7096ed10df08c4d1a9208416dd87fd7
Giorgy-dev <marco.gorgetti.2@gmail.com>
2022-09-20 20:50:09 +0200

    added carousel demo

--- lib/pages/intro.dart
+++ lib/pages/intro.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:hunt_frontend/components/develop/fullnavigator.dart';
 
-import 'package:hunt_frontend/components/carusel_steps.dart';
 import 'package:hunt_frontend/components/screen.dart';
+
+import '../components/CarouselSlider.dart';
 
 class IntroScreen extends StatefulWidget {
   const IntroScreen({super.key});
@@ -15,18 +15,9 @@ class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Screen(
-        text: 'Intro',
-        child: const Center(
-          child:
-          //*FOR DEVELOPMENT
-            FLNavigator(),
-            
-            //bottomNavigationBar: CarouselSteps(steps: 5),
-          )
-      )
-      
-      
-    );
+        body: Screen(
+            text: 'Intro', child: const Center(
+              child: ManuallyControlledSlider()
+            )));
   }
 }
